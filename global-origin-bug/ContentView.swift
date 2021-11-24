8645570d5aa087203551c65e58e0cf9065773e01
Patrick Dinger <121539+paxos@users.noreply.github.com>
2021-11-24 08:02:43 -1000

    Initial Commit

---
+++ global-origin-bug/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  global-origin-bug
+//
+//  Created by Patrick Dinger on 11/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ global-origin-bug/global_origin_bugApp.swift
@@ -0,0 +1,17 @@
+//
+//  global_origin_bugApp.swift
+//  global-origin-bug
+//
+//  Created by Patrick Dinger on 11/24/21.
+//
+
+import SwiftUI
+
+@main
+struct global_origin_bugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
